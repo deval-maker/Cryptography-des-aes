82edbb0984ceecefb5bf1b45bb25e3a3fca0ba5c
deval-maker <deval.maker@gmail.com>
2015-04-22 12:20:35 +0200

    AES Working with file input and output.

--- aes.c
+++ aes.c
@@ -1,42 +1,35 @@
+// to compile >> gcc aes.c -lm -lcrypto -lssl -o aes
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <openssl/aes.h>
 #include <openssl/rand.h>
+#define USAGE   "Usage: aes <input file> <encryption file> <decryption file> <key length [only 128 or 192 or 256!]> \n"
 
-// a simple hex-print routine. could be modified to print 16 bytes-per-line
-static void hex_print(const void* pv, size_t len)
-{
-    const unsigned char * p = (const unsigned char*)pv;
-    if (NULL == pv)
-        printf("NULL");
-    else
-    {
-        size_t i = 0;
-        for (; i<len;++i)
-            printf("%02X", *p++);
-    }
-    printf("\n");
-}
-
-// main entrypoint
 int main(int argc, char **argv)
 {
-    int keylength;
-    FILE *in,*out;
+    int keylength,i;
+    FILE *in,*out,*encr,*decr;
     char ch;
-    printf("Give a key length [only 128 or 192 or 256!]:\n");
-    scanf("%d", &keylength);
+    // printf("Give a key length [only 128 or 192 or 256!]:\n");
+    // scanf("%d", &keylength);
 
+    if (argc != 5) {
+        printf(USAGE);
+        return(1);
+    }
+
+    keylength = atoi(argv[4]);
     /* generate a key with a given length */
     unsigned char aes_key[keylength/8];
     memset(aes_key, 0, keylength/8);
     if (!RAND_bytes(aes_key, keylength/8))
         exit(-1);
 
-    // Get Input 
+    // buffers for encryption and decryption
+    
     size_t inputslength = 0;
-    in = fopen("input.txt", "rb");  //in = fopen(argv[2], "rb");
+    in = fopen(argv[1], "rb");
     if (!in) {
         printf("File not found %s\n", argv[2]);
         return(1);
@@ -44,54 +37,44 @@ int main(int argc, char **argv)
     fseek(in, 0L, SEEK_END);
     inputslength = ftell(in);
     rewind( in );
-    /* generate input with a given length */
-    unsigned char aes_input[inputslength];
-    while (!feof(in)) {
-        int i=0;
-        ch = fgetc(in);
-        aes_input[i]=ch;
-        i++;
-    }
-    //memset(aes_input, 'X', inputslength);
 
-    /* init vector */
+    const size_t encslength = (((inputslength + AES_BLOCK_SIZE) / AES_BLOCK_SIZE) * AES_BLOCK_SIZE );
+    
+    unsigned char enc_out[encslength];
+    unsigned char dec_out[encslength];
+    unsigned char aes_input[encslength];
+    memset(enc_out, '\0', sizeof(enc_out));  // NULL character paddings
+    memset(dec_out, '\0', sizeof(dec_out));
+    memset(aes_input, '\0', sizeof(aes_input));
+    
+    // Get Input 
+    for (i=0; i<inputslength;i++) {
+        aes_input[i] = fgetc(in);
+    }
+
     unsigned char iv_enc[AES_BLOCK_SIZE], iv_dec[AES_BLOCK_SIZE];
     RAND_bytes(iv_enc, AES_BLOCK_SIZE);
     memcpy(iv_dec, iv_enc, AES_BLOCK_SIZE);
-
-    // buffers for encryption and decryption
-    const size_t encslength = ((inputslength + AES_BLOCK_SIZE) / AES_BLOCK_SIZE) * AES_BLOCK_SIZE;
-    unsigned char enc_out[encslength];
-    unsigned char dec_out[inputslength];
-    memset(enc_out, 0, sizeof(enc_out));
-    memset(dec_out, 0, sizeof(dec_out));
-
-    // so i can do with this aes-cbc-128 aes-cbc-192 aes-cbc-256
-    AES_KEY enc_key, dec_key;
-    AES_set_encrypt_key(aes_key, keylength, &enc_key);
-    AES_cbc_encrypt(aes_input, enc_out, inputslength, &enc_key, iv_enc, AES_ENCRYPT);
-
-    out = fopen("encrypt.txt", "wb+");
-    fwrite(enc_out, sizeof(char), sizeof(enc_out), out);
-    fclose(out);
-    printf("Encryption Done. \n");
     
+    //Encryption 
+    AES_KEY enc_key;
+    AES_set_encrypt_key(aes_key, keylength, &enc_key);
+    AES_cbc_encrypt(aes_input, enc_out, encslength, &enc_key, iv_enc, AES_ENCRYPT);
+
+    encr = fopen(argv[2], "wb+");
+    fwrite(enc_out, sizeof(unsigned char), sizeof(enc_out), encr);
+    fclose(encr);
+    printf("Encryption Done. \n");
+
+    // Decryption 
+    AES_KEY dec_key;
     AES_set_decrypt_key(aes_key, keylength, &dec_key);
     AES_cbc_encrypt(enc_out, dec_out, encslength, &dec_key, iv_dec, AES_DECRYPT);
 
-    out = fopen("decrypt.txt", "wb+");
-    fwrite(dec_out, sizeof(char), sizeof(dec_out), out);
-    fclose(out);
+    decr = fopen(argv[3], "wb+");
+    fwrite(dec_out, sizeof(unsigned char), inputslength, decr);
+    fclose(decr);
     printf("Decryption Done. \n");
-
-    // printf("original:\t");
-    // hex_print(aes_input, sizeof(aes_input));
-
-    // printf("encrypt:\t");
-    // hex_print(enc_out, sizeof(enc_out));
-
-    // printf("decrypt:\t");
-    // hex_print(dec_out, sizeof(dec_out));
 
     return 0;
 }
